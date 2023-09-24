fb41a0c9fc9aca53f5cea6e1a3ce55e7c431b140
fad16papa <fad16papa@gmail.com>
2023-09-24 13:45:11 +0800

    Commit changes

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -1,5 +1,6 @@
 using AuctionService.Data;
 using AuctionService.DTOs;
+using AuctionService.Entities;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -35,6 +36,22 @@ namespace AuctionService.Controllers
             if (auction == null) return NotFound();
 
             return _mapper.Map<AuctionDto>(auction);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto auctionDto)
+        {
+            var auction = _mapper.Map<Auction>(auctionDto);
+            //TODO: add current user as seller
+            auction.Seller = "Test";
+
+            _auctionDbContext.Auctions.Add(auction);
+
+            var result = await _auctionDbContext.SaveChangesAsync() > 0;
+
+            if (!result) return BadRequest("Could not save changes to the database");
+
+            return CreatedAtAction(nameof(GetAuctionById), new { auction.Id }, _mapper.Map<AuctionDto>(auction));
         }
     }
 }
