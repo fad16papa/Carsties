0a350d6ed7cbe89a78c23c2e1184f31685e4a568
fad16papa <fad16papa@gmail.com>
2023-09-24 14:20:02 +0800

    Commit changes

--- src/AuctionService/DTOs/UpdateAuctionDto.cs
+++ src/AuctionService/DTOs/UpdateAuctionDto.cs
@@ -4,19 +4,12 @@ namespace AuctionService.DTOs
 {
     public class UpdateAuctionDto
     {
-        [Required]
         public string Make { get; set; }
-        [Required]
         public string Model { get; set; }
-        [Required]
-        public int Year { get; set; }
-        [Required]
+        public int? Year { get; set; }
         public string Color { get; set; }
-        [Required]
-        public int Mileage { get; set; }
-        [Required]
+        public int? Mileage { get; set; }
         public string ImageUrl { get; set; }
-        [Required]
         public int ReservePrice { get; set; }
     }
 }
