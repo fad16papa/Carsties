0b2222f2b8a40a894eb40e7a280901b43be3fbe4
fad16papa <fad16papa@gmail.com>
2023-10-04 23:52:44 +0800

    Commit changes

--- src/SearchService/RequestsHelpers/SearchParams.cs
+++ src/SearchService/RequestsHelpers/SearchParams.cs
@@ -5,5 +5,9 @@ namespace SearchService.RequestsHelpers
         public string SearchTerm { get; set; }
         public int PageNumber { get; set; } = 1;
         public int PageSize { get; set; } = 4;
+        public string Seller { get; set; }
+        public string Winner { get; set; }
+        public string OrderBy { get; set; }
+        public string FilterBy { get; set; }
     }
 }
