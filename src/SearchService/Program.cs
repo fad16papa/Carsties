99e87149db40a75b1d595eee4a8a87078cc79b21
fad16papa <fad16papa@gmail.com>
2023-10-05 01:13:57 +0800

    Commit changes

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -2,14 +2,15 @@ using MongoDB.Driver;
 using MongoDB.Entities;
 using SearchService.Data;
 using SearchService.Models;
+using SearchService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddHttpClient<AuctionServiceHttpClient>();
 
 var app = builder.Build();
 
