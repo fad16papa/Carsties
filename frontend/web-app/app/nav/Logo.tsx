c547bc89c7be7985ba2e54c65cd189ef7e48174d
fad16papa <fad16papa@gmail.com>
2024-01-07 00:44:23 +0800

    Commit changes

--- frontend/web-app/app/nav/Logo.tsx
+++ frontend/web-app/app/nav/Logo.tsx
@@ -1,14 +1,23 @@
 "use client";
 
 import { userParamsStore } from "@/hooks/useParamsStore";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 import { FaCar } from "react-icons/fa";
 
 export default function Logo() {
+  const router = useRouter();
+  const pathName = usePathname();
   const reset = userParamsStore((state) => state.reset);
+
+  function doReset() {
+    if (pathName !== "/") router.push("/");
+    reset();
+  }
+
   return (
     <div
-      onClick={reset}
+      onClick={doReset}
       className="cursor-pointer flex items-center gap-2 text-3xl font-semibold text-red-500"
     >
       <FaCar size={34} />
